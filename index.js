3d3456d054a42ea863336dd4bb6255e920100399
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-19 16:30:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function dwarfRollCall(dwarves) {
+  var hiHo = [];
+  for ( var i = 0; i < dwarves.length; i++)
+  {
+   hiHo.push(`${i+1}. ${dwarves[i]} `);
+  }
+return hiHo.join('');
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+for (var i = 0; i < planeteerCalls.length; i++) 
+ {
+  planeteerCalls[i] = planeteerCalls[i].toUpperCase() + '!';
+ }
+  return planeteerCalls;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++);{
+    if (words.length > 4)
+    return true;
+  }
+  return false;
+  
 }
 
 function findTheCheese (foods) {
+  var lunch = ['cheddar', 'gouda', 'camembert'];
+  for (var i = 0; i < foods.length; i++) { 
+    for (var j = 0; j < lunch.length; j++){
+    
+    if (lunch[j] === foods[i]){
+      return foods[i];
+    }
+    
+    
+}
+}
+return 'no cheese!';
 }
